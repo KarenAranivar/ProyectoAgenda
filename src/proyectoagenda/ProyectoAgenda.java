bc2aa4540b7ad6bdd40bab3fff987ad872383888
karan <karan@LAPTOP-HTBLT890>
2020-08-10 10:33:09 -0500

    karen

---
+++ src/proyectoagenda/ProyectoAgenda.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoagenda;
+
+/**
+ *
+ * @author karan
+ */
+public class ProyectoAgenda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String Agenda=":D";
+        
+        System.out.println("Hola Mari"
+               );
+        System.out.println("Agenda"+ Agenda
+               );
+        
+    }
+    
+}
